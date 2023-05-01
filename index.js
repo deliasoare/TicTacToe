d7444fd0be4495b813a1778f50e901f98d08b2ee
dristosun <107073369+dristosun@users.noreply.github.com>
2023-05-01 16:11:39 +0300

    State that works without smartAI

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+let gameRunning = true;
+let winner = '';
 const Player = (number, piece, name, type)  => {
     const getNumber =  () =>  number;
     const getPiece = () => piece;
@@ -6,7 +8,7 @@ const Player = (number, piece, name, type)  => {
     return {getNumber, getPiece, getName, getType};
 }
 const gameBoard = (() => {
-    const board = Array(3).fill(null);
+    let board = Array(3).fill(null);
     let emptySpaces = [];
     for (let i = 0; i < board.length; i++) 
         board[i] = Array(3).fill(null);
@@ -18,8 +20,9 @@ const gameBoard = (() => {
 
     const getEmptySpaces = () => emptySpaces;
 
+    const numEmptySpaces = () => emptySpaces.length;
+
     const changeEmptySpaces = (numberRemoved) => {
-        console.log(emptySpaces);
         for (i = 0; i < emptySpaces.length; i++)
             if(emptySpaces[i] === numberRemoved) 
                 emptySpaces.splice(i, 1);
@@ -28,12 +31,23 @@ const gameBoard = (() => {
     const addPiece = (number, player) => {
             number = Number(number);
             changeEmptySpaces(number);
-            console.log({emptySpaces, number});
             let column = (number % 3);
             let row = parseInt(number / 3);
             // console.log({row, column})
-            board[row][column] = {piece: player.getPiece(), number : number};
-
+            board[row][column] = player.getPiece();
+    }
+    
+    const clearBoard = () => {
+        board = Array(3).fill(null);
+        emptySpaces = [];
+        for (let i = 0; i < board.length; i++) 
+            board[i] = Array(3).fill(null);
+        for (i = 0; i < 9; i++)
+            emptySpaces.push(i);
+        document.querySelectorAll('.piece').forEach(piece => {
+            piece.remove();
+        })
+        outcome.style.display = 'none';
     }
     
     const checkForEnd = () => {
@@ -44,11 +58,11 @@ const gameBoard = (() => {
             let diagonalCheck = 0, diagMarker;
             let diagonalCheck2 = 0, diagMarker2;
             for (j = 0; j < board.length - 1; j++) {
-                if (board[i][j] === board[i][j+1] && board[i][j] !== null) {
+                if (board[i][j] === board[i][j+1]&& board[i][j] !== null) {
                     iCheck++;
                     iMarker = board[i][j];
                 }
-    
+                console.log({ i, j, board: board[j][i] });
                 if (board[j][i] === board[j+1][i] && board[j][i] !== null) {
                     jCheck++;
                     jMarker = board[j][i];
@@ -64,10 +78,10 @@ const gameBoard = (() => {
                     diagMarker2 = board[j][board.length - j - 1];
             }
 
-            if (iCheck === 2)  return displayController.end('won', iMarker);
-            else if (jCheck === 2) return displayController.end('won', jMarker);
-            else if (diagonalCheck === 2) {return displayController.end('won', diagMarker);}
-            else if (diagonalCheck2 === 2) {return displayController.end('won', diagMarker2)};
+            if (iCheck === 2)  {displayController.end('won', iMarker); return iMarker}
+            else if (jCheck === 2) {displayController.end('won', jMarker); return jMarker; }
+            else if (diagonalCheck === 2) {displayController.end('won', diagMarker); return diagMarker;}
+            else if (diagonalCheck2 === 2) {displayController.end('won', diagMarker2); return diagMarker2};
 
         }
 
@@ -82,7 +96,7 @@ const gameBoard = (() => {
         }, 0);
 
     }
-    return {getBoard, getEmptySpaces, addPiece, checkForEnd};
+    return {getBoard, getEmptySpaces, addPiece, checkForEnd, clearBoard, numEmptySpaces};
 })();
 
 const displayController = (() => {
@@ -90,11 +104,13 @@ const displayController = (() => {
         if (document.querySelector('.startScreen').style.display === 'none') {
             document.querySelector('.startScreen').style.display = 'flex';
             document.querySelector('.cont').style.display = 'none';
+            document.querySelector('.gameScreen').style.display = 'none';
             start.style.display = 'block';
         }
         else {
             document.querySelector('.startScreen').style.display = 'none';
             document.querySelector('.cont').style.display = 'flex';
+            document.querySelector('.gameScreen').style.display = 'block';
             start.style.display = 'none';
         }
     }
@@ -108,9 +124,8 @@ const displayController = (() => {
 
 
     const addToScreen = (number, player) => {
-        
         const element = document.createElement('div');
-        element.classList = player.getPiece() === 'X' ? 'X piece' : 'O piece';
+        element.classList += player.getPiece() === 'X' ? 'X piece' : 'O piece';
         element.textContent = player.getPiece() === 'X' ? 'X' : 'O';
         pieces[number].append(element);
     }
@@ -119,12 +134,16 @@ const displayController = (() => {
             piece.removeEventListener('click', play);
         })
     }
-    const end = (state, sign = 'none') => {
+    const end = (state, sign = '') => {
+        gameRunning = false;
+        winner = sign;
         // console.log({state, sign});
-        cancelClick();
-        const outcome = document.querySelector('.outcome');
+        outcome.style.display = 'block';
+        restart.style.display = 'block';
         if (state === 'won') outcome.textContent = sign === playerOne.getPiece() ? `${playerOne.getName()} has won! Congratulations!` : `${playerTwo.getName()} has won! Congratulations!`;
         else outcome.textContent = 'This is a draw.'
+        cancelClick();
+        
     }
 
     const playRound = (number) => {
@@ -137,20 +156,104 @@ const displayController = (() => {
             }, 0);
     }
 
-    const playGame = () => {
-        if (getActivePlayer().getType() === '"Player"') {
-            pieces.forEach(piece => {
-                    piece.addEventListener('click', play, {once : true});
-            });
-        }
+    const randomChoice = () => {
+        number = gameBoard.getEmptySpaces()[Math.floor((Math.random() * gameBoard.getEmptySpaces().length))];
+        playRound(number);
+    }
+
+    const minimax = (crrBdState, mark) => {
+        let emptySpots = gameBoard.getEmptySpaces();
+        const other_player = mark === 'X' ? 'O' : 'X';
+
+        let result = gameBoard.checkForEnd();
+        if (emptySpots.length === 0)
+            return 0;
+        if (result)
+            if (result === mark)
+                return 1;
+            else
+                return -1;
+        
+        const allTestPlayInfos = [];
+
+        for (let i = 0; i < emptySpots.length; i++) {
+            const currentTestPlay = {};
+            currentTestPlay.index = crrBdState[emptySpots[i]];
+            crrBdState[emptySpots[i]] = mark;
+            let res;
+            let otherMark = mark === 'X' ? 'O' : 'X';
+            if (mark === getActivePlayer().getPiece()) {
+                 res = minimax(crrBdState, otherMark);
+                 currentTestPlay.score = res;
+            }
+            else if (mark === other_player) {
+                res = minimax(crrBdState, mark);
+                currentTestPlay.score = res;
+            }
+
+            crrBdState[emptySpots[i]] = currentTestPlay.index;
+
+            allTestPlayInfos.push(currentTestPlay);
+
+          }
+
+
+            let bestTestPlay =  null;
+
+            let bestScore;
+
+            if (mark === getActivePlayer().getPiece()) {
+                bestScore = -1000;
+                for (let i = 0; i < allTestPlayInfos.length; i++) {
+                    if (allTestPlayInfos[i].score > bestScore) {
+                        bestScore = allTestPlayInfos[i].score;
+                        bestTestPlay = i;
+                    }
+                }
+            }
+            else if (mark === other_player) {
+                bestScore = 1000;
+                for (let i = 0; i < allTestPlayInfos.length; i++) {
+                    if (allTestPlayInfos[i].score < bestScore) {
+                        bestScore = allTestPlayInfos[i].score;
+                        bestTestPlay = i;
+                    }
+                }
+            }
+
+            return bestTestPlay;
+
+    }
+
+    const smartAI = () => {
+        cancelClick();
+        if (gameBoard.getEmptySpaces() === 9)
+            randomChoice();
         else {
             cancelClick();
-            number = gameBoard.getEmptySpaces()[Math.floor(Math.random() * gameBoard.getEmptySpaces().length) + 1];
-            
+            number = minimax(gameBoard.getBoard(), getActivePlayer().getPiece());
             playRound(number);
         }
     }
-
+    const weakAI = () => {
+        cancelClick();
+        setTimeout(function() {
+            randomChoice();
+            }, 1000);
+    }   
+   
+    const playGame = () => {
+        if (gameRunning) 
+            if (getActivePlayer().getType() === '"Player"') {
+                pieces.forEach(piece => {
+                        if (piece.innerHTML === '')
+                            piece.addEventListener('click', play, {once : true});
+                });
+            }
+            else {
+                smartAI();   
+            }
+    }
     return {playRound, end, switchDisplay, getActivePlayer, playGame, switchPlayerTurn};
 })();
 
@@ -162,8 +265,13 @@ function play(piece) {
 
 const pieces = document.querySelectorAll('.lilSquare');
 const start = document.querySelector('#submit');
+const restart = document.querySelector('#restart');
+const bttns = [start, restart];
+const outcome = document.querySelector('.outcome');
 
-start.addEventListener('click', function() {
+
+bttns.forEach(bttn => {
+    bttn.addEventListener('click', function() {
     const type1 = getComputedStyle(document.querySelector('#type1'),'::before').getPropertyValue('content');
     const type2 = getComputedStyle(document.querySelector('#type2'),'::before').getPropertyValue('content');
 
@@ -178,6 +286,15 @@ start.addEventListener('click', function() {
     displayController.switchPlayerTurn(playerOne, playerTwo);
 
     displayController.playGame();
-    
+    })
+})
+
+start.addEventListener('click', () => {
+    restart.style.display = 'none';
+})
+
+restart.addEventListener('click', function() {
+    gameRunning = true;
+    gameBoard.clearBoard();
 })
 
